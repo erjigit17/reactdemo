45ce1ef44a0d4237d0c5223d8d1539224a6c6900
Эржигит Имамов <erjigit17@gmail.com>
2021-10-08 16:15:05 +0600

    invokeIf

--- tmp.js
+++ tmp.js
@@ -226,29 +226,29 @@
 //   (value > max ? value : max), 0)
 
 // console.log('Max age: ', maxAge)
-const colors = [
-  {
-    id: "xekare",
-    title: "rad red",
-    rating: 3
-}, {
-    id: "jbwsof",
-    title: "big blue",
-    rating: 2
-}, {
-    id: "prigbj",
-    title: "grizzly grey",
-    rating: 5
-}, {
-    id: "ryhbhsl",
-    title: "banana",
-    rating: 1
-} ]
+// const colors = [
+//   {
+//     id: "xekare",
+//     title: "rad red",
+//     rating: 3
+// }, {
+//     id: "jbwsof",
+//     title: "big blue",
+//     rating: 2
+// }, {
+//     id: "prigbj",
+//     title: "grizzly grey",
+//     rating: 5
+// }, {
+//     id: "ryhbhsl",
+//     title: "banana",
+//     rating: 1
+// } ]
 
-const hashColors = colors.reduce((hash, {id, title, rating}) => {
-  hash[id] = { title, rating}
-  return hash
-}, {})
+// const hashColors = colors.reduce((hash, {id, title, rating}) => {
+//   hash[id] = { title, rating}
+//   return hash
+// }, {})
 // console.log(hashColors)
 
 
@@ -270,3 +270,16 @@ const hashColors = colors.reduce((hash, {id, title, rating}) => {
 // }
 
 // console.log(arrToObj(array))
+// const colors = ["red", "red", "green", "blue", "green"]
+// const uniqueColors = colors.reduce((unique, color) => unique.indexOf(color) !== -1 ? unique : [...unique, color], [])
+// console.log(uniqueColors)
+
+const invokeIf = (condition, fnTrue, fnFalse) =>
+  condition ? fnTrue() : fnFalse()
+
+const showWelcome = () => console.log('Welcome!!!')
+
+const showUnauthorized = () => console.log('Unauthorized!!!')
+
+invokeIf(true, showWelcome, showUnauthorized)
+invokeIf(false, showWelcome, showUnauthorized)
