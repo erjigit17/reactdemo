0785b9db63d39d3866c6fa07e0e6ea77c4822578
Эржигит Имамов <erjigit17@gmail.com>
2021-10-08 13:55:40 +0600

    start comput data

--- tmp.js
+++ tmp.js
@@ -151,3 +151,25 @@
 // const addColor = (title, list) => [...list, {title}]
 // console.log(addColor('Glam Green', list).length)
 // console.log(list.length)
+// const frederick = {
+//   name: "Frederick Douglass",
+//   canRead: false,
+//   canWrite: false
+// };
+// const selfEducate = person => ({
+//   ...person,
+//   canRead: true,
+//   canWrite: true
+// })
+// console.log(selfEducate(frederick));
+// console.log(frederick);
+
+// function Header(text) {
+//   let h1 = document.createElement('h1')
+//   h1.innerHTML = text
+//   document.body.appendChild(h1)
+// }
+// const Header = props => <h1>{props.title}</h1>
+
+// Header('Header() caused side effects')
+const schools = ["Yorktown", "Washington & Liberty", "Wakefield"];
