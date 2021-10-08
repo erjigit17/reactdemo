bcaf0ff676035804a7dbff486a7766a4b9baf623
Эржигит Имамов <erjigit17@gmail.com>
2021-10-08 10:53:28 +0600

    add classes

--- tmp.js
+++ tmp.js
@@ -1,2 +1,25 @@
-const person = (firstName, lastName) => ({ first: firstName, last: lastName });
-console.log(person('Erji', 'Im'))
+// function Vacation(destination, length) {
+//   this.destination = destination
+//   this.length = length
+// }
+
+// Vacation.prototype.print = function() {
+//   console.log(this.destination + ' | ' + this.length + ' days')
+// }
+
+// const maui = new Vacation('Maui', 7)
+// maui.print()
+class Vacation {
+  constructor(destination, length) {
+    this.destination = destination
+    this.length = length
+  }
+
+  print() {
+    console.log(`${this.destination} will take ${this.length} days.`)
+  }
+}
+
+const trip = new Vacation('Santiago, Chile', 7)
+
+trip.print()
