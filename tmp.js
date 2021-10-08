8af8859527bfad6193e66fa959524b66261507a4
Эржигит Имамов <erjigit17@gmail.com>
2021-10-08 17:35:55 +0600

    create clock

--- tmp.js
+++ tmp.js
@@ -316,26 +316,68 @@
 // }
 // const log = value => console.log(value)
 // countdown(10, log)
-const deepPick = (fields, object = {}) => {
-  const [first, ...remaining] =fields.split('.')
-  return remaining.length
-    ? deepPick(remaining.join('.'), object[first])
-    : object[first]
+// const deepPick = (fields, object = {}) => {
+//   const [first, ...remaining] = fields.split(".");
+//   return remaining.length
+//     ? deepPick(remaining.join("."), object[first])
+//     : object[first];
+// };
+
+// const dan = {
+//   type: "person",
+//   data: {
+//     gender: "male",
+//     info: {
+//       id: 22,
+//       fullname: {
+//         first: "Dan",
+//         last: "Deacon"
+//       }
+//     }
+//   }
+// }
+
+// deepPick("type", dan)
+// deepPick("data.info.fullname.first", dan)
+
+setInterval(logClockTime, 1000)
+
+function logClockTime() {
+  let time = getClockTime()
+  console.clear()
+  console.log(time)
 }
 
-const dan = {
-  type: 'person',
-  data: {
-    gender: 'male',
-    info: {
-      id: 22,
-      fullname: {
-        fist: 'Dan',
-        last: 'Deacon'
-      }
-    }
+function getClockTime() {
+  let date = new Date()
+
+  let time = {
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+    ampm: 'AM'
   }
+
+  if (time.hours == 12) {
+    time.ampm = 'PM'
+  } else if (time.hours > 12) {
+      time.ampm = 'PM'
+      time.hours -= 12
+  }
+
+  if (time.hours < 10) {
+    time.hours = '0' + time.hours
+  }
+
+  if (time.minutes < 10) {
+    time.minutes = '0' + time.minutes
+  }
+
+  if (time.seconds < 10) {
+    time.seconds = '0' + time.seconds
+  }
+
+  return time.hours + ':' + time.minutes + ':' + time.seconds + ' ' + time.ampm
 }
 
-deepPick('type', dan)
-deepPick('data.info.fullname.fist', dan)
+logClockTime()
