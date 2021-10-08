bc32ef6c5cba2f127296ad09a1a27988b889cc33
Эржигит Имамов <erjigit17@gmail.com>
2021-10-08 13:07:33 +0600

    save befor clean func

--- tmp.js
+++ tmp.js
@@ -106,12 +106,48 @@
 // wrapper.appendChild(headline)
 // target.appendChild(wrapper)
 
-const { render } = ReactDom
+// const { render } = ReactDom
 
-const Welcome = () => (
-  <div id="welcome">
-    <h1>Hello World</h1>
-  </div>
-)
+// const Welcome = () => (
+//   <div id="welcome">
+//     <h1>Hello World</h1>
+//   </div>
+// )
 
-render(<Welcome />, document.getElementById('target'))
+// render(<Welcome />, document.getElementById('target'))
+// let color_lawn = {
+//   title: 'lawn',
+//   color: '#00ff00',
+//   rating: 0
+// }
+// function rateColor(color, rating) {
+//   color.rating = rating
+//   return color
+// }
+
+// console.log(rateColor(color_lawn, 5).rating)
+// console.log(color_lawn.rating)
+
+// const rateColor =  function (color, rating) {
+//   return Object.assign({}, color, {rating: rating })
+// }
+
+// const rateColor = (color, rating) => ({
+//   ...color,
+//   rating
+// })
+// console.log(rateColor(color_lawn, 5).rating)
+// console.log(color_lawn.rating)
+
+// let list = [{title: 'Rad Red'}, {title: 'Lawn'}, {title: 'Party Pink'}]
+
+// const addColor = function(title, colors) {
+//   colors.push({ title: title})
+//   return colors
+// }
+
+// const addColor = (title, array) => array.concat({title})
+
+// const addColor = (title, list) => [...list, {title}]
+// console.log(addColor('Glam Green', list).length)
+// console.log(list.length)
